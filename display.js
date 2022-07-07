799802b19d45d9f47cd1e510142013a030a15afd
LopezWellsSebastian <sebastianwells33@hotmail.com>
2022-07-06 20:38:53 -0700

    1.3 (assignment 3)
    We created a table that reacts to the use of the button Register displaying the newer data.
    We make all the form have a form o validation

---
+++ display.js
@@ -0,0 +1,44 @@
+function displayCardPets(){
+    console.clear();
+    let card = " ";
+    //travel tha array of cards
+    for(let i = 0; i < petSalon.pets.length; i++)
+    {
+        console.log(petSalon.pets[i].name);
+        //create the cards (HTML)
+        let pet=petSalon.pets[i];
+        card += `<div class="pet">
+                    <h4> ${pet.name}<h4>
+                    <p>Age: ${pet.age}</p>
+                    <p>Gender: ${pet.gender}</p>
+                    <p>Breed: ${pet.breed}</p>
+                    <p>Service: ${pet.service}</p>
+                    <p>Owners: ${pet.ownerName}</p>
+                    <p>Phone: ${pet.contactPhone}</p>
+                </div>`;
+                console.log(card);
+    }
+    //inject to the html
+    document.getElementById('pet-cards').innerHTML = card;
+}
+
+function displayTables() {
+    let table = "";
+    for(let i = 0; i < petSalon.pets.length; i++)
+    {
+        let tablePet=petSalon.pets[i];
+        let id= i+1;
+        table += ` 
+                        <tr>
+                            <th scope="row">${id}</th>
+                            <td>${tablePet.name}</td>
+                            <td>${tablePet.age}</td>
+                            <td>${tablePet.gender}</td>
+                            <td>${tablePet.breed}</td>
+                            <td>${tablePet.service}</td>
+                            <td>${tablePet.ownerName}</td>
+                            <td>${tablePet.contactPhone}</td>
+                        </tr>` 
+    }
+    document.getElementById('tbody').innerHTML = table;
+}
--- register.js
+++ register.js
@@ -28,15 +28,16 @@ function displayInfo()
 {
     document.getElementById("infor").innerHTML = `Welcome to the ${petSalon.name} family. It is available at ${petSalon.address} from ${petSalon.hours.open} to ${petSalon.hours.close}`;
 }
-
+/*
 function displayConsole()
 {
+    console.clear();
     for(let i=0; i<petSalon.pets.length; i++)
     {
         console.log(petSalon.pets[i].name);
     }
 
-}
+}*/
 
 //displayAlert where we use the alert to display how many pets we have registered.
 function displayAlert()
@@ -48,21 +49,28 @@ function displayNumberPets(){
     document.getElementById("pets-number").innerHTML = `The number of pets we are displaying are: ${petSalon.pets.length}`
 }
 
-function cleanDisplayNumberPets(){
-        document.getElementById("pets-list").innerHTML = ``;
-    
-}
 
 function clearInputs(){
      //get info from the inputs
-     document.getElementById("txtPetName").value="";
-     document.getElementById("txtPetAge").value="";
-     document.getElementById("txtPetGender").value="";
-     document.getElementById("txtPetBreed").value="";
-     document.getElementById("txtPetService").value=document.getElementById("op1").value;
-     document.getElementById("txtPetOName").value="";
-     document.getElementById("txtPetContact").value="";
+    document.getElementById("txtPetName").value="";
+    document.getElementById("txtPetAge").value="";
+    document.getElementById("txtPetGender").value="";
+    document.getElementById("txtPetBreed").value="";
+    document.getElementById("txtPetService").value=document.getElementById("op1").value;
+    document.getElementById("txtPetOName").value="";
+    document.getElementById("txtPetContact").value="";
 }
+
+function isValid(aPet){
+    let valid=true;
+    if(aPet.name=="" || aPet.age=="" || aPet.gender=="" || aPet.breed==""||aPet.ownerName=="" || aPet.contactPhone==""){//is not empty?
+        valid=false;
+        console.log("valid = false");
+    }
+    return valid;
+}
+
+    
 function register(){
     //get info from the inputs
     let petName=document.getElementById("txtPetName").value;
@@ -73,15 +81,21 @@ function register(){
     let petOwnerName=document.getElementById("txtPetOName").value;
     let petContact=document.getElementById("txtPetContact").value;
     //create a obj
-    let pet = new Pet(petName, petAge, petGender, petBreed, petService, petOwnerName, petContact);
-    //push obj
-    petSalon.pets.push(pet);
-
+    let newPet = new Pet(petName, petAge, petGender, petBreed, petService, petOwnerName, petContact);
+    if(isValid(newPet))
+    {
+        //push obj
+        petSalon.pets.push(newPet);
         displayInfo();
-        displayConsole();
-        cleanDisplayNumberPets()
+        //displayConsole();
         displayNumberPets();
         clearInputs();
+        //displayCardPets();
+        displayTables();
+    }
+    else{
+        alert("Please enter the information again");
+    }
 }
 
 
@@ -94,16 +108,18 @@ for(let i=0; i<petSalon.pets.length; i++)
 
 function init(){
     //creating objects
-        let scooby = new Pet("Scooby", 12, "Male", "Cutting Nails", "Sebastian", "664-820-4622");
-        let scrappy = new Pet("Scrappy", 6, "Male", "Bathing", "Sebastian", "664-820-4622");
-        let peabody = new Pet("Peabody", 12, "Male", "Cutting Nails", "Sherman", "664-820-4622");
+        let scooby = new Pet("Scooby", 12, "Male", "Stray Dog","Cutting Nails", "Sebastian", "664-820-4622");
+        let scrappy = new Pet("Scrappy", 6, "Male", "Stray Dog","Bathing", "Sebastian", "664-820-46222");
+        let peabody = new Pet("Peabody", 12, "Male","Stray Dog", "Cutting Nails", "Sherman", "664-820-46223");
 
         petSalon.pets.push(scooby,scrappy,peabody);
     //main function
         //calling the functions
         displayInfo();
-        displayConsole();
+        //displayConsole();
         displayNumberPets();
+        //displayCardPets();
+        displayTables();
     //hook events
 
 }
